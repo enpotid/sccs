bdc138ca7e98d06809b5202d0498a19d57b71600
owo404 <xian101221@gmail.com>
2025-03-16 01:02:40 +0900

    add sub

--- src/sctype/mod.rs
+++ src/sctype/mod.rs
@@ -1,7 +1,5 @@
 pub mod scint;
-pub mod scuint;
 pub mod util;
 
 pub use scint::*;
-pub use scuint::*;
 pub use util::*;
--- src/sctype/scint.rs
+++ src/sctype/scint.rs
@@ -1,11 +1,12 @@
-use std::fmt;
+use std::{cmp::Ordering, fmt, ops};
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 pub enum Sign {
     Plus,
     Minus,
 }
 
+#[derive(PartialEq, Eq, Clone)]
 pub struct ScInt {
     pub sign: Sign,
     pub value: Vec<u32>,
@@ -57,6 +58,202 @@ impl ScInt {
             sign: if x >= 0 { Sign::Plus } else { Sign::Minus },
             value: list,
         }
+    }
+}
+
+impl ops::Add for ScInt {
+    type Output = ScInt;
+
+    fn add(self, other: ScInt) -> ScInt {
+        if self.sign == other.sign {
+            let a: Vec<u64> = self.value.iter().rev().map(|&s| s as u64).collect();
+            let b: Vec<u64> = other.value.iter().rev().map(|&s| s as u64).collect();
+
+            let mut result = Vec::new();
+            let mut up = 0;
+            for i in 0..a.len().max(b.len()) {
+                let x = a.get(i).unwrap_or(&0) + b.get(i).unwrap_or(&0) + up;
+                if x > u32::MAX as u64 {
+                    result.push((x - u32::MAX as u64 - 1) as u32);
+                    up = 1;
+                } else {
+                    result.push(x as u32);
+                    up = 0;
+                }
+            }
+            if up != 0 {
+                result.push(up as u32);
+            }
+
+            loop {
+                if let Some(x) = result.pop() {
+                    if x != 0 {
+                        result.push(x);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            result.reverse();
+            ScInt {
+                sign: self.sign,
+                value: result,
+            }
+        } else {
+            let mut ns = self.clone();
+            ns.sign = Sign::Plus;
+            let mut no = other.clone();
+            no.sign = Sign::Plus;
+            if ns > no {
+                ScInt {
+                    sign: self.sign,
+                    value: (ns - no).value,
+                }
+            } else if ns < no {
+                ScInt {
+                    sign: other.sign,
+                    value: (no - ns).value,
+                }
+            } else {
+                ScInt {
+                    sign: Sign::Plus,
+                    value: Vec::new(),
+                }
+            }
+        }
+    }
+}
+
+impl ops::Sub for ScInt {
+    type Output = ScInt;
+
+    fn sub(self, other: ScInt) -> ScInt {
+        if self.sign == other.sign {
+            if self.sign == Sign::Plus {
+                if self > other {
+                    let a: Vec<i64> = self.value.iter().rev().map(|&s| s as i64).collect();
+                    let b: Vec<i64> = other.value.iter().rev().map(|&s| s as i64).collect();
+
+                    let mut result = Vec::new();
+                    let mut down = 0;
+                    for i in 0..a.len().max(b.len()) {
+                        let x = a.get(i).unwrap_or(&0) - b.get(i).unwrap_or(&0) - down;
+                        if x < 0 {
+                            result.push((x + u32::MAX as i64 + 1) as u32);
+                            down = 1;
+                        } else {
+                            result.push(x as u32);
+                            down = 0;
+                        }
+                    }
+
+                    loop {
+                        if let Some(x) = result.pop() {
+                            if x != 0 {
+                                result.push(x);
+                                break;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+
+                    result.reverse();
+                    ScInt {
+                        sign: Sign::Plus,
+                        value: result,
+                    }
+                } else if self < other {
+                    let mut n = other - self;
+                    n.sign = Sign::Minus;
+                    n
+                } else {
+                    ScInt {
+                        sign: Sign::Plus,
+                        value: Vec::new(),
+                    }
+                }
+            } else {
+                let mut ns = self;
+                ns.sign = Sign::Plus;
+                let mut no = other;
+                no.sign = Sign::Plus;
+                no - ns
+            }
+        } else {
+            if self.sign == Sign::Plus {
+                let mut no = other;
+                no.sign = Sign::Plus;
+                self + no
+            } else {
+                let mut no = other;
+                no.sign = Sign::Minus;
+                self + no
+            }
+        }
+    }
+}
+
+impl Ord for ScInt {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self == other {
+            Ordering::Equal
+        } else {
+            if self.sign != other.sign {
+                if self.sign == Sign::Plus {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            } else {
+                let mut ns: Vec<u32> = self.value.clone().iter().rev().map(|&s| s).collect();
+                loop {
+                    if let Some(x) = ns.pop() {
+                        if x != 0 {
+                            ns.push(x);
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                let mut no: Vec<u32> = other.value.clone().iter().rev().map(|&s| s).collect();
+                loop {
+                    if let Some(x) = no.pop() {
+                        if x != 0 {
+                            no.push(x);
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                if ns.len() > no.len() {
+                    Ordering::Greater
+                } else if ns.len() < no.len() {
+                    Ordering::Less
+                } else {
+                    for i in 0..ns.len() {
+                        if ns[i] > no[i] {
+                            return Ordering::Greater;
+                        } else if ns[i] < no[i] {
+                            return Ordering::Less;
+                        }
+                    }
+                    Ordering::Equal
+                }
+            }
+        }
+    }
+}
+
+impl PartialOrd for ScInt {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
