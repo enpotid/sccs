b60353fb6a566fb6e48f20c077e6c9516dcc9be3
owo404 <xian101221@gmail.com>
2025-03-20 16:36:18 +0900

    plf

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
 use sctype::scint::ScInt;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 mod sctype;
 
-pub fn add(left: &str, right: &str) -> ScInt {
-    sctype::scint::ScInt::from(left).unwrap() + sctype::scint::ScInt::from(right).unwrap()
+pub fn fibonacci(n: ScInt) -> ScInt {
+    let dp: Arc<Mutex<HashMap<ScInt, ScInt>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    {
+        let mut dp_lock = dp.lock().unwrap();
+        dp_lock.insert(ScInt::from_i128(0), ScInt::from_i128(0));
+        dp_lock.insert(ScInt::from_i128(1), ScInt::from_i128(1));
+        dp_lock.insert(ScInt::from_i128(2), ScInt::from_i128(1));
+    }
+
+    fib(&dp, &n)
+}
+
+fn fib(dp: &Arc<Mutex<HashMap<ScInt, ScInt>>>, n: &ScInt) -> ScInt {
+    {
+        let dp_lock = dp.lock().unwrap();
+        if let Some(x) = dp_lock.get(n) {
+            return x.clone();
+        }
+    }
+
+    let x = fib(dp, &(n - ScInt::from_i128(1))) + fib(dp, &(n - ScInt::from_i128(2)));
+    let mut dp_lock = dp.lock().unwrap();
+    dp_lock.insert(n.clone(), x.clone());
+
+    x
 }
 
 #[cfg(test)]
@@ -12,12 +38,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let a = sctype::scint::ScInt::from("19929234843272348973248974328978342").unwrap();
-        let b = sctype::scint::ScInt::from("274535830479054983728427509345302789").unwrap();
-        let result = a + b;
-        assert_eq!(
-            result,
-            sctype::scint::ScInt::from("294465065322327332701676483674281131").unwrap()
-        );
+        let a = fibonacci(ScInt::from_i128(1000));
+        assert_eq!(a, sctype::scint::ScInt::from("43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875").unwrap());
     }
 }
